baebb1c33af0bdbc34b0a6874ac33d5c76768588
Pablo Puerta <jpuerta523@gmail.com>
2023-01-16 14:22:53 -0500

    palabra + larga aprobada fcc

---
+++ app.js
@@ -0,0 +1,19 @@
+
+/* identificar elementos HTML */
+const input = document.getElementById("inlet")
+const btn = document.getElementById("calculate")
+const resultado = document.querySelector(".alert-success")
+
+//const str="The quick brown fox jumped over the lazy dog";
+function strMaxLeng(str){
+    let maxLeng  = str.split(" ").map(word => word.length).reduce(function(a, b) {
+        return Math.max(a, b);
+    },-Infinity);
+    return maxLeng
+}
+
+/* boton Calcular  */
+btn.addEventListener("click",()=>{
+    let ans = input.value
+    resultado.textContent = `La palabra más larga de la oración: "${ans}" tiene ${strMaxLeng(ans)} caracteres.`       
+})
