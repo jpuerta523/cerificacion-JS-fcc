4e5772ebcd41d435fcbfb7d4f91c371d62835064
Pablo Puerta <jpuerta523@gmail.com>
2023-01-21 10:04:42 -0500

    confirmar final aprobado FCC

--- 4. cadena mas larga/app.js
+++ 4. cadena mas larga/app.js
@@ -1,10 +1,14 @@
+/* Cadena mas larga, actividad de certificacion freecodecamp
+estilos realizados con bootstrap y logica en JS
+modificado para presentar resultados en documento HTML con respecto a lo solicitado por fcc */
 
 /* identificar elementos HTML */
 const input = document.getElementById("inlet")
 const btn = document.getElementById("calculate")
 const resultado = document.querySelector(".alert-success")
 
-//const str="The quick brown fox jumped over the lazy dog";
+/* funcion para encontrar el nuemro de caracteres de cadena mas larga*/
+/* priemro vuelve la cadena en un arreglo, luego recorre cada posicion del arreglo y busca el maximo */
 function strMaxLeng(str){
     let maxLeng  = str.split(" ").map(word => word.length).reduce(function(a, b) {
         return Math.max(a, b);
---
+++ 6. confirmar final/app.js
@@ -0,0 +1,46 @@
+/* Confirmar final, actividad de certificacion freecodecamp
+estilos realizados con bootstrap y logica en JS
+modificado para presentar resultados en documento HTML con respecto a lo solicitado por fcc */
+
+/* identificar elementos HTML */
+const input1 = document.getElementById("inlet1")
+const input2 = document.getElementById("inlet2")
+const btn = document.getElementById("calculate")
+const resultado = document.querySelector(".alert-success")
+const resultadoFalso = document.querySelector(".alert-danger")
+
+
+/* funcion para comfirmar final */
+function confirmEnding(str, target){
+    let targetLen =  target.length
+    let strEnd = str.slice(str.length-targetLen, str.length)
+    return target === strEnd
+}
+
+/* boton Calcular  */
+btn.addEventListener("click",()=>{
+
+    /* limpia los resultados anteriores */
+    resultado.classList.add("d-none")
+    resultadoFalso.classList.add("d-none")
+
+    let str = input1.value
+    let strEnd = input2.value
+    let ans = confirmEnding(str, strEnd)
+
+    /* maneja los resultados para true y false */
+    if (ans===true){
+        resultado.classList.remove("d-none")
+        resultado.textContent = `la palabra ${str} efectivamente finaliza con ${strEnd}`
+    }else{
+        resultadoFalso.classList.remove("d-none")
+        resultadoFalso.textContent = `la palabra ${str} no finaliza con ${strEnd}`
+    }
+    
+    /* resultado.textContent = ` ${confirmEnding(str, strEnd)}`  */      
+})
+
+
+
+
+
