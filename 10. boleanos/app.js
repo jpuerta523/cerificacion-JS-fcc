bfcd79d0c40a8ffbc0102481ce8fd20e1f925ac8
Pablo Puerta <jpuerta523@gmail.com>
2023-01-25 11:16:11 -0500

    BooWho aprobado FCC

---
+++ 10. boleanos/app.js
@@ -0,0 +1,10 @@
+/* BooWho, actividad de certificacion freecodecamp
+estilos realizados con bootstrap y logica en JS
+modificado para presentar resultados en documento HTML con respecto a lo solicitado por fcc */
+
+let sentence = true
+
+/* logica para verificar si es booleano */
+function booWho(sentence){
+    return typeof sentence === "boolean"
+}
