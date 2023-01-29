b7f25853d44e498e3b8e78ce70c993a8a2d5bdbe
Pablo Puerta <jpuerta523@gmail.com>
2023-01-29 12:20:46 -0500

    Rebote Falsy aprobado FCC

---
+++ 13.falsy/app.js
@@ -0,0 +1,12 @@
+
+let arr = [7, "ate", "", false, 9]
+
+/*filtra de un arreglo los elementos boleanos */
+function bouncer(arr){
+    return arr.filter(Boolean)
+}
+
+console.log(bouncer(arr))
+
+
+    
